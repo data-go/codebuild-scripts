ef020cb2061c23939de2716c32d253f8b4d5b163
miagx <overwhenn@gmail.com>
2018-12-12 19:24:39 -0800

    Delete branch_name.sh

--- branch_name.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CODEBUILD_GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
-if [ "$CODEBUILD_GIT_BRANCH" = "" ] ; then
-  CODEBUILD_GIT_BRANCH="$(git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }')";
-  export CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/};
-fi
